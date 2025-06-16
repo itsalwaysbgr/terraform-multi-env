f427ef9568e47658b542526c2e4a7f232c22ddb1
nane <gnane5940@gmail.com>
2025-06-16 16:00:09 +0530

    2025-06-16

---
+++ tfvars/variables.tf
@@ -0,0 +1,36 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+
+
+variable "common_tags" {
+  default = {
+    Project   = "roboshop"
+    Terraform = "true"
+  }
+}
+
+/* variable "final-name" {
+    default = "${var.project}-${var.environment}-${var.component}"
+} */
+
+# ec2 name = cart
+# ec2 name = roboshop-dev-cart
+
+variable "sg_name" {
+  default= "allow-all"
+}
+
+variable "sg_description" {
+  default="Allowing all ports from all IP's"
+}
+
+variable "instances" {
+  default=["mongodb","redis"]
+  
+}
